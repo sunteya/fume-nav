94366e212d01c4a28914dc89f1407cd2549d0659
sunteya <sunteya@gmail.com>
2017-06-02 00:13:05 +0800

    add link_to helper method

--- lib/fume/nav/nav_tag.rb
+++ lib/fume/nav/nav_tag.rb
@@ -18,20 +18,19 @@ module Fume
         content_tag(value, :li, options, &block)
       end
 
+      def link_to(value, *args, &block)
+        options = args.extract_options!
+        apply_active_options(value, options)
+        helper.link_to(*args, options, &block)
+      end
+
       def content_tag(value, tag_name, options = {}, &block)
         apply_active_options(value, options)
         helper.content_tag(tag_name, options, &block)
       end
 
       def apply_active_options(value, options)
-        match = case value
-                when Regexp
-                  value.match(current)
-                else
-                  value.to_s == current.to_s
-                end
-
-        if match
+        if match?(value)
           if options[:class]
             options[:class] += " #{active_class}"
           else
@@ -40,6 +39,15 @@ module Fume
         end
 
         @empty = false
+      end
+
+      def match?(value)
+        case value
+        when Regexp
+          value.match(current)
+        else
+          value.to_s == current.to_s
+        end
       end
     end
   end
